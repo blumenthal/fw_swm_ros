834dcdf3ab283440b52d6465c71ce21f441df363
Timo Hinzmann <Timo.Hinzmann@mavt.ethz.ch>
2016-12-04 10:07:15 +0100

    Added license text.

--- include/swm_ros/swm_ros.h
+++ include/swm_ros/swm_ros.h
@@ -1,3 +1,21 @@
+// This file is part of fw_swm_ros.
+//
+// Copyright (C) 2016 Timo Hinzmann <hitimo at ethz dot ch> 
+// (Autonomous Systems Lab, ETH Zurich, Switzerland)
+//
+// fw_swm_ros is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// fw_swm_ros is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with fw_swm_ros.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef SWM_ROS_H_
 #define SWM_ROS_H_
 
